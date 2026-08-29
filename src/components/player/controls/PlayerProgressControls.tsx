703e9fd2abf232284d589a6ad0739bf3cfb25936
bluedust <bluedvst@gmail.com>
2026-08-29 10:28:29 +0800

    refactor(ios): remove fixed-height player control stack

--- src/components/player/controls/PlayerProgressControls.tsx
+++ src/components/player/controls/PlayerProgressControls.tsx
@@ -1,9 +1,9 @@
+import { StyleSheet } from 'react-native';
 import { useActiveTrack } from 'react-native-track-player';
 import Animated, { useAnimatedStyle } from 'react-native-reanimated';
 
 import { Progress } from './ProgressBars/Progress';
 import { PlayerControls } from './PlayerControls';
-import { styles } from '@components/style';
 
 export default function PlayerProgressControls({
   opacity,
@@ -11,15 +11,22 @@ export default function PlayerProgressControls({
 }: NoxComponent.OpacityProps) {
   const track = useActiveTrack();
 
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: opacity.value,
-    };
-  });
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }));
+
   return (
-    <Animated.View style={[styles.actionRowContainer, animatedStyle, style]}>
+    <Animated.View style={[styles.container, animatedStyle, style]}>
       <Progress live={track?.isLiveStream} />
       <PlayerControls />
     </Animated.View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+});
