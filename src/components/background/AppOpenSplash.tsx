c770d7605c79ba28e5b97be11c024f73442b0118
bluedust <bluedvst@gmail.com>
2026-08-29 10:21:59 +0800

    refactor(ios): remove remote promotional splash content

--- src/components/background/AppOpenSplash.tsx
+++ src/components/background/AppOpenSplash.tsx
@@ -1,112 +1,47 @@
 import React from 'react';
-import { Dimensions, Pressable, View } from 'react-native';
+import { Pressable, useWindowDimensions } from 'react-native';
 import { Image } from 'expo-image';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useVideoPlayer, VideoView } from 'expo-video';
-import { useEventListener } from 'expo';
 
-import { randomChoice } from '@utils/Utils';
-import useTanakaAmazingCommodities from '@hooks/useTanakaAmazingCommodities';
 import useAlert from '../dialogs/useAlert';
 import { clearStorage } from '@utils/ChromeStorageAPI';
 
 enum SplashType {
   Image = 'image',
-  Video = 'video',
-  Tanaka = 'tanaka',
 }
 
 type SplashArray = [SplashType, () => unknown][];
 
+/** Local artwork retained for the appearance preview screen. No remote splash
+ * content or promotional video is requested during application startup. */
 export const imageSplashes: SplashArray = [
   [SplashType.Image, () => require('@assets/splash/steria2.jpg')],
   [SplashType.Image, () => require('@assets/splash/nox-3d.jpg')],
   [SplashType.Image, () => require('@assets/splash/nox-3d-2024.jpg')],
 ];
 
-const localSplashes: SplashArray = [
-  ...imageSplashes,
-  [SplashType.Tanaka, () => require('@assets/splash/nox-3d.jpg')],
-];
-
-const randomSplash = randomChoice(localSplashes);
-
-interface TanakaProps {
-  url: string;
-  onEnd: () => void;
-}
-const TanakaAmazingCommodities = ({ url, onEnd }: TanakaProps) => {
-  const { width, height } = Dimensions.get('window');
-  const insets = useSafeAreaInsets();
-  const fullScreenStyle = {
-    width,
-    height: height + insets.top + insets.bottom,
-    flex: 1,
-  };
-  const player = useVideoPlayer({ uri: url }, player => {
-    player.audioMixingMode = 'mixWithOthers';
-    player.volume = 0.7;
-    player.play();
-  });
-
-  useEventListener(player, 'playToEnd', onEnd);
-
-  return (
-    <>
-      <VideoView
-        player={player}
-        style={[{ position: 'absolute' }, fullScreenStyle]}
-        contentFit="cover"
-        buttonOptions={{
-          showBottomBar: false,
-          showPlayPause: false,
-          showSeekBackward: false,
-          showSeekForward: false,
-          showSettings: false,
-        }}
-        nativeControls={false}
-      />
-      <Pressable style={fullScreenStyle} onPress={onEnd} />
-    </>
-  );
-};
-
 interface Props {
   setIsSplashReady: (v: boolean) => void;
 }
-const AppOpenSplash = ({ setIsSplashReady }: Props) => {
-  const { tanaka, initialized } = useTanakaAmazingCommodities();
-  const { width, height } = Dimensions.get('window');
-  const insets = useSafeAreaInsets();
-  const fullScreenStyle = {
-    width,
-    height: height + insets.top + insets.bottom,
-    flex: 1,
-  };
 
-  switch (randomSplash[0]) {
-    case SplashType.Tanaka:
-      if (!initialized) return <View />;
-      if (tanaka) {
-        return (
-          <TanakaAmazingCommodities
-            url={tanaka}
-            onEnd={() => setIsSplashReady(true)}
-          />
-        );
-      }
-    // @eslint-disable-next-line no-fallthrough
-    case SplashType.Image:
-    default:
-      setTimeout(() => setIsSplashReady(true), 1);
-      return (
-        <Image
-          source={randomSplash[1]() as number}
-          style={fullScreenStyle}
-          contentFit={'contain'}
-        />
-      );
-  }
+const AppOpenSplash = ({ setIsSplashReady }: Props) => {
+  const { width, height } = useWindowDimensions();
+  const insets = useSafeAreaInsets();
+
+  React.useEffect(() => {
+    setIsSplashReady(true);
+  }, [setIsSplashReady]);
+
+  return (
+    <Image
+      source={imageSplashes[0][1]() as number}
+      style={{
+        width,
+        height: height + insets.top + insets.bottom,
+      }}
+      contentFit="contain"
+    />
+  );
 };
 
 export default function AppOpenSplashView(p: Props) {
