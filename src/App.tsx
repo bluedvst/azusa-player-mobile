201e442790a66f6a977497c687172c7b8e3bb966
bluedust <bluedvst@gmail.com>
2026-08-29 10:42:11 +0800

    fix: format iPhone launch surface

--- src/App.tsx
+++ src/App.tsx
@@ -78,12 +78,7 @@ export default function App(appProps: NoxComponent.AppProps) {
   if (!isPlayerReady) {
     return (
       <SafeAreaProvider>
-        <View
-          style={[
-            styles.launchContainer,
-            { backgroundColor: appBackground },
-          ]}
-        />
+        <View style={[styles.launchContainer, { backgroundColor: appBackground }]} />
       </SafeAreaProvider>
     );
   }
