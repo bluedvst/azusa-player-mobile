098e9d3b060da1edddfde16c3575fcecc4bc0bd8
bluedust <bluedvst@gmail.com>
2026-08-29 11:51:05 +0800

    Polish iPhone landscape player panel

--- src/components/landscape/LandscapePlayerPanel.tsx
+++ src/components/landscape/LandscapePlayerPanel.tsx
@@ -1,21 +1,37 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useActiveTrack } from 'react-native-track-player';
 import React from 'react';
 
 import TrackInfoTemplate from '../player/TrackInfo/TrackInfoTemplate';
 import LandscapePlayerProgress from './LandscapePlayerProgress';
+import { useNoxSetting } from '@stores/useApp';
+import { isIOS } from '@utils/RNUtils';
 
 interface Props {
   panelWidth: number;
 }
+
 export default function LandscapePlayerPanel({ panelWidth }: Props) {
   const track = useActiveTrack();
+  const playerStyle = useNoxSetting(state => state.playerStyle);
+
   return (
-    <View style={{ justifyContent: 'flex-end', width: panelWidth }}>
+    <View
+      style={[
+        styles.container,
+        {
+          width: panelWidth,
+          backgroundColor: playerStyle.colors.background,
+        },
+      ]}
+    >
       <TrackInfoTemplate
         track={track}
         windowWidth={panelWidth}
-        containerStyle={{ flex: 1, justifyContent: 'center' }}
+        containerStyle={[
+          styles.trackInfo,
+          isIOS && styles.trackInfoIOS,
+        ]}
       >
         <></>
       </TrackInfoTemplate>
@@ -23,3 +39,18 @@ export default function LandscapePlayerPanel({ panelWidth }: Props) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'flex-end',
+    overflow: 'hidden',
+  },
+  trackInfo: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  trackInfoIOS: {
+    paddingHorizontal: 16,
+    paddingTop: 8,
+  },
+});
