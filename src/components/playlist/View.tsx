3b29908483b2a91ce2cf733c22b4c8a40dbaaa04
bluedust <bluedvst@gmail.com>
2026-08-29 10:28:39 +0800

    refactor(ios): make playlist screen edge-safe and full-width

--- src/components/playlist/View.tsx
+++ src/components/playlist/View.tsx
@@ -1,26 +1,32 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import { styles } from '../style';
 import BiliSearchbar from './BiliSearch/BiliSearchbar';
 import usePlaylist from './usePlaylistRN';
 import SongList from './SongList/View';
 import MenuSheet from './MenuSheet';
 import { useNoxSetting } from '@stores/useApp';
 import { useIsLandscape } from '@hooks/useOrientation';
+import { isIOS } from '@utils/RNUtils';
 
 const Playlist = () => {
   const isLandscape = useIsLandscape();
   const insets = useSafeAreaInsets();
   const currentPlaylist = useNoxSetting(state => state.currentPlaylist);
+  const playerStyle = useNoxSetting(state => state.playerStyle);
   const usedPlaylist = usePlaylist(currentPlaylist);
 
   return (
     <View
       style={[
-        styles.contentContainer,
-        { paddingTop: isLandscape ? insets.top / 2 : insets.top },
+        styles.container,
+        {
+          paddingTop: isLandscape ? insets.top / 2 : insets.top,
+          backgroundColor: isIOS
+            ? playerStyle.colors.background
+            : 'transparent',
+        },
       ]}
     >
       <MenuSheet usedPlaylist={usedPlaylist} />
@@ -31,3 +37,11 @@ const Playlist = () => {
 };
 
 export default Playlist;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'stretch',
+  },
+});
